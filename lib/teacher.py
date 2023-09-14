57785e755e5b33dda3f8d0a9b9a95e92726fec43
Juan Ortiz-Acosta <jdoa2003@gmail.com>
2023-09-14 11:48:39 -0600

    Actually finished lab

--- lib/teacher.py
+++ lib/teacher.py
@@ -8,7 +8,16 @@ class Teacher(User):
 
     def __init__(self, first_name, last_name):
         super().__init__( first_name, last_name)
-        self.knowledge = ["Math", "Science"]
+        self.knowledge = knowledge = [
+            "str is a data type in Python",
+            "programming is hard, but it's worth it",
+            "JavaScript async web request",
+            "Python function call definition",
+            "object-oriented teacher instance",
+            "programming computers hacking learning terminal",
+            "pipenv install pipenv shell",
+            "pytest -x flag to fail fast",
+        ]
 
     def teach(self):
-        return "Math"
+        return self.knowledge[random.randint(0, len(self.knowledge) - 1)]
